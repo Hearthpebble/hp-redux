fe4efd10dcc6dbad318867844a1ff2bdf22f5098
Eric <eric.ebersohl.1@gmail.com>
2016-12-18 15:59:19 -0600

    updtated heroes reducer for new state format

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@
 const { combineReducers } = require('redux');
 const game = require('./game');
 const players = require('./players');
+const heroes = require('./heroes');
 
 const rootReducer = combineReducers({
   game,
   players,
+  heroesById: heroes,
 });
 
 module.exports = rootReducer;
