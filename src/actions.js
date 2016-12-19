f60a223b54e80883ed3ef941e0377f72122b8bd8
Brandon <brandon.ebersohl@gmail.com>
2016-12-18 22:51:39 -0600

    ADD_PLAYER action

--- src/actions.js
+++ src/actions.js
@@ -1,1 +1,13 @@
 /* eslint-env node*/
+const shortid = require('shortid');
+
+const ADD_PLAYER = exports.ADD_PLAYER = 'ADD_PLAYER';
+
+exports.addPlayer = (playerClass, deck, name) => ({
+  type: ADD_PLAYER,
+  playerId: shortid.generate(),
+  heroId: shortid.generate(),
+  playerClass,
+  deck,
+  name,
+});
