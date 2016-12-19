94417ae76b1ab949d2f5b37545ae14d0c2e0d4c5
Eric <eric.ebersohl.1@gmail.com>
2016-12-18 20:07:59 -0600

    Added Deck Cards reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,9 +4,11 @@ const { combineReducers } = require('redux');
 const game = require('./game');
 const players = require('./players');
 const heroes = require('./heroes');
+const deckCards = require('./deckCards');
 
 const rootReducer = combineReducers({
   game,
+  deckCardsByIds: deckCards,
   playersById: players,
   heroesById: heroes,
 });
