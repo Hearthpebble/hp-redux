8b15eb19304218d6c4b23aad1acd87df8666dd86
Kevin Thomas <kevinthomas2710@gmail.com>
2016-12-21 15:51:16 -0600

    Added secret reducer (#17)
    * Added secret reducer
    
    * Fixed style error

---
+++ src/reducers/secrets.js
@@ -0,0 +1,12 @@
+/* eslint-env node*/
+
+const initialState = {};
+
+const secrets = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+module.exports = secrets;
