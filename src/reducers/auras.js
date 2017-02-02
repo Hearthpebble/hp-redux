898dccedb95170eb76d741b70b9e02a826a8d71f
Kristin Schleif <Kristin.Schleif@intelligentinsites.com>
2017-02-02 14:02:50 -0600

    added auras reducer (#54)

---
+++ src/reducers/auras.js
@@ -0,0 +1,12 @@
+/* eslint-env node*/
+
+const initialState = {};
+
+const auras = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+module.exports = auras;
